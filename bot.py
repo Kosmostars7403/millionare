c6d3615842e75f548b3224f01043894df0ca74d6
ivancernakov <3699818@mail.ru>
2020-05-03 17:56:28 +0300

    Первая версия игры

--- bot.py
+++ bot.py
@@ -4,10 +4,30 @@ import random
 import requests
 import os
 import json
+import redis
 
 token = os.environ["TELEGRAM_TOKEN"]
+REDIS_URL = os.environ['REDIS_URL']
+
+
 bot = telebot.TeleBot(token)
 telebot.apihelper.proxy = {'https': 'socks5://stepik.akentev.com:1080'}
+
+data = {}
+
+def save_values(key,value):
+    if REDIS_URL:
+        redis_db = redis.drom_url(REDIS_URL)
+        redis_db.set(key, value)
+    else:
+        data[key] = value
+
+def load_values(key, default):
+    if REDIS_URL:
+        redis_db = redis.drom_url(REDIS_URL)
+        return redis_db.get(key, default)
+    else:
+        return data.get(key, default)
 
 scores = {'victories': 0, 'defeats': 0}
 
@@ -17,7 +37,7 @@ COMPLEXITY_CHOOSE = 'complexity'
 
 #data = {'states': {}, 'user_complexity':{}, MAIN_STATE:{}, QUESTION_ASK:{}, COMPLEXITY_CHOOSE:{}}
 
-data = json.load(open('db/data.json', 'r', encoding='utf-8'))
+#data = json.load(open('db/data.json', 'r', encoding='utf-8'))
 
 def change_data(key, user_id, value):
     data[key][user_id] = value
@@ -30,9 +50,10 @@ keyboard_main = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one
 keyboard_main.add(types.KeyboardButton('Задай вопрос!'), types.KeyboardButton('Покажи счет.'),
                   types.KeyboardButton('Поменять сложность.'))
 
-@bot.message_handler(func=lambda message: data['states'].get(str(message.from_user.id), MAIN_STATE) == MAIN_STATE)
+#@bot.message_handler(func=lambda message: data['states'].get(str(message.from_user.id), MAIN_STATE) == MAIN_STATE)
+@bot.message_handler(func=lambda message: load_values('state: {user_id}'.format(user_id = message.from_user.id), MAIN_STATE) == MAIN_STATE)
 def main_handler(message):
-    comp = data['user_complexity'].get(str(message.from_user.id), 1)
+    comp = load_values('complexity: {user_id}'.format(user_id=message.from_user.id), 1)
     global QUESTIONS
     QUESTIONS = requests.get('https://stepik.akentev.com/api/millionaire', params={'complexity': comp}).json()
     print(message)
@@ -43,12 +64,13 @@ def main_handler(message):
         answers_array = QUESTIONS['answers']
         array = []
         for var in answers_array:
-            array.append(types.KeyboardButton(var))
+            array.append(types.KeyboardButton(var)) 1
         random.shuffle(array)
         keyboard.add(*array)
         bot.reply_to(message, QUESTIONS['question'], reply_markup=keyboard)
         #data['states'][message.from_user.id] = QUESTION_ASK
-        change_data('states', str(message.from_user.id), QUESTION_ASK)
+        #change_data('states', str(message.from_user.id), QUESTION_ASK)
+        save_values('state: {user_id}'.format(user_id = message.from_user.id), QUESTION_ASK)
     elif 'покажи счет.' in message.text.lower():
         bot.reply_to(message, 'Побед: ' + str(scores.get('victories')) + ' Поражений: ' + str(scores.get('defeats')))
     elif 'поменять сложность.' in message.text.lower():
@@ -58,51 +80,61 @@ def main_handler(message):
         bot.reply_to(message, 'Выбери сложность вопросов, сейчас сложность: ' + str(comp),
                      reply_markup=keyboard_complexity)
         #data['states'][message.from_user.id] = COMPLEXITY_CHOOSE
-        change_data('states', str(message.from_user.id), COMPLEXITY_CHOOSE)
+        #change_data('states', str(message.from_user.id), COMPLEXITY_CHOOSE)
+        save_values('state: {user_id}'.format(user_id=message.from_user.id), COMPLEXITY_CHOOSE)
     else:
         bot.reply_to(message, 'я тебя не понял')
 
 
-@bot.message_handler(func=lambda message: data['states'].get(str(message.from_user.id), MAIN_STATE) == QUESTION_ASK)
+@bot.message_handler(func=lambda message: load_values('state: {user_id}'.format(user_id = message.from_user.id), MAIN_STATE) == QUESTION_ASK)
 def question_ask(message):
     print(message)
     if message.text == QUESTIONS['answers'][0]:
         bot.reply_to(message, 'Правильно!', reply_markup=keyboard_main)
         scores['victories'] += 1
         #data['states'][message.from_user.id] = MAIN_STATE
-        change_data('states', str(message.from_user.id), MAIN_STATE)
+        #change_data('states', str(message.from_user.id), MAIN_STATE)
+        save_values('state: {user_id}'.format(user_id=message.from_user.id), MAIN_STATE)
     else:
         bot.reply_to(message, 'Неправильно :(', reply_markup=keyboard_main)
         scores['defeats'] += 1
         #data['states'][message.from_user.id] = MAIN_STATE
-        change_data('states', str(message.from_user.id), MAIN_STATE)
+        #change_data('states', str(message.from_user.id), MAIN_STATE)
+        save_values('state: {user_id}'.format(user_id=message.from_user.id), MAIN_STATE)
 
 
-@bot.message_handler(func=lambda message: data['states'].get(str(message.from_user.id), MAIN_STATE) == COMPLEXITY_CHOOSE)
+@bot.message_handler(func=lambda message: load_values('state: {user_id}'.format(user_id = message.from_user.id), MAIN_STATE) == COMPLEXITY_CHOOSE)
 def complexity(message):
     print(message)
     if message.text == '1 сложность':
         bot.reply_to(message, 'Продолжим с вопросами 1-ой сложности?', reply_markup=keyboard_main)
         #data['user_complexity'][message.from_user.id] = 1
         #data['states'][message.from_user.id] = MAIN_STATE
-        change_data('states', str(message.from_user.id), MAIN_STATE)
-        change_data('user_complexity', str(message.from_user.id), 1)
+        #change_data('states', str(message.from_user.id), MAIN_STATE)
+        #change_data('user_complexity', str(message.from_user.id), 1)
+        save_values('state: {user_id}'.format(user_id=message.from_user.id), MAIN_STATE)
+        save_values('complexity: {user_id}'.format(user_id=message.from_user.id), 1)
     elif message.text == '2 сложность':
         bot.reply_to(message, 'Продолжим с вопросами 2-ой сложности?', reply_markup=keyboard_main)
         #data['user_complexity'][message.from_user.id] = 2
         #data['states'][message.from_user.id] = MAIN_STATE
-        change_data('states', str(message.from_user.id), MAIN_STATE)
-        change_data('user_complexity', str(message.from_user.id), 2)
+        #change_data('states', str(message.from_user.id), MAIN_STATE)
+        #change_data('user_complexity', str(message.from_user.id), 2)
+        save_values('state: {user_id}'.format(user_id=message.from_user.id), MAIN_STATE)
+        save_values('complexity: {user_id}'.format(user_id=message.from_user.id), 2)
     elif message.text == '3 сложность':
         bot.reply_to(message, 'Продолжим с вопросами 3-ей сложности?', reply_markup=keyboard_main)
         #data['user_complexity'][message.from_user.id] = 3
         #data['states'][message.from_user.id] = MAIN_STATE
-        change_data('states', str(message.from_user.id), MAIN_STATE)
-        change_data('user_complexity', str(message.from_user.id), 3)
+        #change_data('states', str(message.from_user.id), MAIN_STATE)
+        #change_data('user_complexity', str(message.from_user.id), 3)
+        save_values('state: {user_id}'.format(user_id=message.from_user.id), MAIN_STATE)
+        save_values('complexity: {user_id}'.format(user_id=message.from_user.id), 3)
     elif message.text == 'Назад.':
         bot.reply_to(message, 'Продолжим с прежней сложностью?', reply_markup=keyboard_main)
         #data['states'][message.from_user.id] = MAIN_STATE
-        change_data('states', str(message.from_user.id), MAIN_STATE)
+        #change_data('states', str(message.from_user.id), MAIN_STATE)
+        save_values('state: {user_id}'.format(user_id=message.from_user.id), MAIN_STATE)
 
 
 bot.polling()
